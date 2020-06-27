cdd46d006d28265ee0c2ba007db28e89c66f12cf
ZeroTixDev <67355595+ZeroTixDev@users.noreply.github.com>
2020-06-26 22:17:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,25 @@ app.get('/',(req,res)=>{
 app.use('/client',express.static(__dirname+'/client'))
 serv.listen(2000)
 console.log("Server Started")
-class Player{
+class Entity{
+  constructor(){
+    this.x = 250;
+    this.y = 250;
+    this.spdX=0;
+    this.spdY = 0;
+    this.id=''
+  }
+  update(){
+    this.updatePosition()
+  }
+  updatePosition(){
+    this.x+=this.spdX;
+    this.y+=this.spdY;
+  }
+}
+class Player extends Entity{
   constructor(id){
+    super();
     this.x = 250;
     this.y = 250;
     this.id =id;
@@ -21,8 +38,13 @@ class Player{
     this.pressingUp = false;
     this.pressingDown = false;
     this.maxSpd = 10;
+    Player.list[id]=this;
   }
-  updatePosition(){
+  update(){
+    this.updateSpd()
+    super.update();
+  }
+  updateSpd(){
     if(this.pressingRight){
       this.x+=this.maxSpd;
     }
@@ -36,44 +58,52 @@ class Player{
       this.y+=this.maxSpd;
     }
   }
+  static pack(){
+    let pack = []
+    for(let i in Player.list){
+      let player = Player.list[i];
+      player.updateSpd()
+      pack.push({
+        x:player.x,
+        y:player.y,
+        number:player.number,
+      })
+    }
+    return pack;
+  }
+  static onConnect(socket){
+    let player = new Player(socket.id)
+    socket.on('keyPress',(data)=>{
+    if(data.inputId ==='left'){
+      player.pressingLeft=data.state;
+    }else if(data.inputId ==='right'){
+      player.pressingRight=data.state;
+    }else if(data.inputId ==='up'){
+      player.pressingUp=data.state;
+    }else if(data.inputId ==='down'){
+      player.pressingDown=data.state;
+    }
+    })
+  }
+  static onDisconnect(socket){
+    delete Player.list[socket.id]
+  }
 }
+Player.list={};
+
 io.sockets.on('connection',(socket)=>{
   socket.id = Math.random()
-  socket.x = 0;
-  socket.y = 0;
-  socket.number = ""+Math.floor(10*Math.random())
   socketList[socket.id] = socket;
-  let player = new Player(socket.id)
-  playerList[socket.id] =  player;
-  console.log('socket connection')
+  Player.onConnect(socket)
   socket.on('disconnect',()=>{
-    delete socketList[socket.id]
-    delete playerList[socket.id]
-  })
-  socket.on('keyPress',(data)=>{
-  if(data.inputId ==='left'){
-    player.pressingLeft=data.state;
-  }else if(data.inputId ==='right'){
-    player.pressingRight=data.state;
-  }else if(data.inputId ==='up'){
-    player.pressingUp=data.state;
-  }else if(data.inputId ==='down'){
-    player.pressingDown=data.state;
-  }
+      delete socketList[socket.id]
+      Player.onDisconnect(socket)
   })
 })
 setInterval(()=>{
-  let pack = []
-  for(let i in playerList){
-    let player = playerList[i];
-    player.updatePosition()
-    pack.push({
-      x:player.x,
-      y:player.y,
-      number:player.number,
-    })
-  }
+  let pack =Player.pack()
   for(let i in socketList){
     let socket = socketList[i]
     socket.emit('newPosition',pack);
-  }},1000/25)
+  }
+},1000/25)
